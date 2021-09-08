e54ac8043aa9ecb8e1073475e88bbf8332359cc3
Dmitry Naumchik <ceqadrat@gmail.com>
2021-09-08 17:56:13 +0300

    Add fonts, replace logo images and add header info

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import { Link } from 'react-router-dom';
 import { Routes } from 'routes';
+import logo from 'static/images/logo.png';
 
 import styles from './Header.module.css';
 
@@ -12,10 +13,13 @@ type Props = {
 const Title: FunctionComponent<Props> = ({ title, subtitle }) => {
   return (
     <header className={styles.header}>
-      <div className={styles.titleContainer}>
-        <h1 className={styles.title}>{title}</h1>
-        <p className={styles.subtitle}>{subtitle}</p>
-      </div>
+      <Link className={styles.logo} to={Routes.main.path}>
+        <img src={logo} alt="React pizza" />
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>{title}</h1>
+          <p className={styles.subtitle}>{subtitle}</p>
+        </div>
+      </Link>
       <div className={styles.cartContainer}>
         <Link to={Routes.cart.path}>Cart</Link>
       </div>
